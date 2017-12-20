f2cae95f6ab21b1471a4dc258ec012447074f736
Sahana Balram <sahana2008balram@gmail.com>
2017-12-20 14:49:53 -0800

    Remove files

--- Always Leave Table In Order/SecondMeal/suspectsInvestigation.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE suspectsInvestigation()
-BEGIN
-	SELECT id,name,surname FROM Suspect 
-    WHERE height <= 170 AND UPPER(name) LIKE 'b%' AND UPPER(surname) LIKE 'gre_n'
-    ORDER BY id;
-END
